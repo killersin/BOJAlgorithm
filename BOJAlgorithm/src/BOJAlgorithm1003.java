3284455fdc3283d64359c1490d4a77b6b66476c1
DESKTOP-MCNC4PH\killershin <d>
2017-11-07 10:07:50 +0900

    알고리즘 업로드

---
+++ BOJAlgorithm/src/BOJAlgorithm1003.java
@@ -0,0 +1,33 @@
+﻿import java.util.Scanner;
+
+//백준 알고리즘 1003번 피보나치 함수  
+public class BOJAlgorithm1003 {
+    private static int fiboZero = 0;
+    private static int fiboOne = 0;
+  
+    public static int fibonacci(int n) {
+        if(n==0){
+            fiboZero++;
+            return 0;
+        }else if(n==1){
+            fiboOne++;
+            return 1;
+        }else
+            return fibonacci(n-1) + fibonacci(n-2);
+    }
+  
+    public static void main(String[] args) {
+      System.out.print("반복할 횟수를 입력하세요 : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i=0; i<num; i++) {
+            fibonacci(sc.nextInt());
+            System.out.println(i+"번째 수"+fiboZero + " "+(i+1)+"번째 수" + fiboOne);
+            //초기화 부분
+            fiboZero = 0;
+            fiboOne = 0;
+        }
+          
+    }
+  
+}
---
+++ BOJAlgorithm/src/BOJAlgorithm1929.java
@@ -0,0 +1,24 @@
+﻿import java.util.Scanner;
+
+//백준 알고리즘 1929번 : 소수 구하기
+public class BOJAlgorithm1929 {
+  public static void main(String[] args) {
+    System.out.print("수를 입력 : ");
+    Scanner sc = new Scanner(System.in);
+    int scInt = sc.nextInt();
+    System.out.print("수를 입력 : ");
+    int scInt2 = sc.nextInt();
+    for(int i=scInt; i<=scInt2; i++){
+      for(int j=2; j<=i; j++){
+        if(i%j==0){
+          if(i==j){
+            System.out.print(i+" ");
+          }else break;
+        }
+      }
+    }
+    System.out.println();
+    System.out.println(scInt+"이상 "+scInt2+"이하의 소수입니다.");
+  }
+   
+}
