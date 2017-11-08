eb3b3744749a6e8b40380286b6762d36c8752a4a
DESKTOP-MCNC4PH\killershin <d>
2017-11-08 10:10:12 +0900

    백준 알고리즘 2675번 : 1,2,3 더하기

---
+++ BOJAlgorithm/src/BOJAlgorithm9095.java
@@ -0,0 +1,23 @@
+﻿import java.util.Scanner;
+/* https://www.acmicpc.net/problem/9095 
+ 백준 알고리즘 9095 1,2,3 더하기 */
+public class BOJAlgorithm9095 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); //방법의 수를 테스트할 갯수
+        int[] arr = new int[11];
+     
+        arr[1]=1; 
+        arr[2]=2;
+        arr[3]=4;
+        //경우의 수 추출
+        for(int i=0; i<T; i++){
+            int n = sc.nextInt();
+            for(int j=4; j<=n; j++){
+                arr[j]=arr[j-3]+arr[j-2]+arr[j-1];
+            }
+            System.out.println(arr[n]);
+        }
+ 
+    }
+}
